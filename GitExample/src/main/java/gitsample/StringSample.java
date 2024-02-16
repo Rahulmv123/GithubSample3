3373de2bd93791b1f725598a7100379da96d948c
pc <pc@DESKTOP-COMFI2C>
2024-02-16 08:41:13 +0530

    SecondCommit

---
+++ GitExample/src/main/java/gitsample/StringSample.java
@@ -0,0 +1,12 @@
+package gitsample;
+
+public class StringSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="First Commit";
+		System.out.println(s);
+
+	}
+
+}
